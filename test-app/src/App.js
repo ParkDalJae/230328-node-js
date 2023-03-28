7bd8e58383f85ca8ffc0a060376171d73156d1b1
ParkDalJae <dv.over931@gmail.com>
2023-03-28 09:52:22 +0900

    react test -개쩖-

---
+++ test-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>안녕하세요</h1>
+      <h1>이거슨 리액트</h1>
+    </div>
+  );
+}
+
+export default App;
